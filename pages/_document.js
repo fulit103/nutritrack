72f63729e05a345ab89268262671cd362671a3c2
Julian Toro <jtoro@tecmovin.com>
2024-07-12 13:25:36 -0500

    chore: Update _document.js to load Typeform script asynchronously

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en" className="scroll-smooth">
-            <Head />
+            <Head>
+                <script async src="//embed.typeform.com/next/embed.js" ></script>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
             </body>
-            <script async src="//embed.typeform.com/next/embed.js" ></script>
         </Html>
     );
 }
