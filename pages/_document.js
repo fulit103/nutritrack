86dfe19bffcbbfdf1938039b1e1057a112592b9a
Julian Toro <jtoro@tecmovin.com>
2024-08-17 10:09:59 -0500

    chore: Update package.json to include @next/third-parties@14.2.5

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export default function Document() {
     return (
         <Html lang="en" className="scroll-smooth">
+            <GoogleTagManager gtmId="GTM-N2K4W69"/>
             <Head />
             <body>
                 <Main />
-                <NextScript />
+                <NextScript />                
             </body>
         </Html>
     );
